18863b1971cdcb9767cc4ab88feb23b34e403b3b
gisele <medina.g.contact@gmail.com>
2026-03-11 22:11:04 -0300

    fix:rm createdAt from update idea schema

--- src/schemas/idea/update-idea.schema.ts
+++ src/schemas/idea/update-idea.schema.ts
@@ -1,11 +1,8 @@
-import z from "zod";
+import z from 'zod'
 
-export const UpdateIdeaSchema=z.object({
-    
-        _id:z.string(),
-        title:z.string().min(3,"too schort").max(30,"maximum of 30 characters").optional(),
-        description:z.string().min(3, " too short ").max(300,"maximum of 300 characters").optional(),
-        createdAt:z.iso.datetime().transform(val=>new Date(val)).optional()
-
-});
-export type UpdateIdeaSchema=z.infer<typeof UpdateIdeaSchema>;
+export const UpdateIdeaSchema = z.object({
+  _id: z.string(),
+  title: z.string().min(3, 'too schort').max(30, 'maximum of 30 characters').optional(),
+  description: z.string().min(3, ' too short ').max(300, 'maximum of 300 characters').optional(),
+})
+export type UpdateIdeaSchema = z.infer<typeof UpdateIdeaSchema>
